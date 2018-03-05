f65e666e50b56b7039d3a1bc3b7d0a52750771fb
horyon <ludovic.noirault@imie.fr>
2018-03-05 16:50:09 +0100

    les autres views

---
+++ app/src/main/java/calvoitmove/projett/CreateChannelActivity.java
@@ -0,0 +1,13 @@
+package calvoitmove.projett;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CreateChannelActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_channel);
+    }
+}
---
+++ app/src/main/java/calvoitmove/projett/SelectChannelActivity.java
@@ -0,0 +1,13 @@
+package calvoitmove.projett;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SelectChannelActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_channel);
+    }
+}
