c670b40b03b8a38629e36ebcf31841e5d9e70698
horyon <ludovic.noirault@imie.fr>
2018-03-19 14:38:21 +0100

    Premiers Json to string, espérons que ça marche

--- app/src/main/java/calvoitmove/projett/CreateChannelActivity.java
+++ app/src/main/java/calvoitmove/projett/CreateChannelActivity.java
@@ -4,6 +4,10 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class CreateChannelActivity extends AppCompatActivity {
 
@@ -13,9 +17,39 @@ public class CreateChannelActivity extends AppCompatActivity {
         setContentView(R.layout.activity_create_channel);
     }
 
-    public void changeToChannel(View view)
-    {
+    public void changeToChannel(View view) throws JSONException {
+        /*
+        * recupérer nom channel depuis le edit text
+        * */
+
+        EditText channelName = findViewById(R.id.champ_nom_salle);
+        String nom_channel = String.valueOf(channelName.getText());
+
+        /*
+        *   Envoyer le username, le password et le mail
+        *   en chaines de caractères (JSON) au seveur
+        */
+
+        JSONObject channelJSON = new JSONObject();
+        JSONObject infoJSON = new JSONObject();
+
+        infoJSON.put("type", "selection");
+        infoJSON.put("username", nom_channel);
+        channelJSON.put("channel",infoJSON);
+
+        String sendableChannel = channelJSON.toString();
+
+
+
+        /*
+        * passer a la view de listes de tous les channels
+        * avec celle qui viens d'être créer
+        * */
         Intent intent = new Intent(CreateChannelActivity.this, ChannelActivity.class);
         startActivityForResult(intent, 123);
+        /*
+        * Ou pas
+        * */
+
     }
 }
